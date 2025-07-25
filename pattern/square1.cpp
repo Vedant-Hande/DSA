c219cee03025394aead6e7abcc61f6dd3fd2b384
vedant hande <vedanthande453@gmail.com>
2025-07-25 21:07:52 +0530

    Starting DSA wiht cpp ---Add square pattern programs in C++
    - Implemented square pattern of asterisks in squar2.cpp
    - Created alphabet square pattern in squar3.cpp
    - Developed numeric square pattern in square1.cpp

---
+++ pattern/square1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i=0;i<4;i++){
+        for(int j=0; j<4;j++){
+            cout << j+1 << " ";
+        }
+        cout << endl;
+    } 
+}
+// This program prints a 4x4 square pattern of numbers from 1 to 4.
