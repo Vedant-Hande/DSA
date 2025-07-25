70e38ff151280a8383364e9754c3fd894225b0dd
vedant hande <vedanthande453@gmail.com>
2025-07-25 22:34:08 +0530

    fead : floyd triangle and reverse triangle

--- pattern/floyd_triangle.cpp
+++ pattern/floyd_triangle.cpp
@@ -4,12 +4,13 @@ using namespace std;
 int main()
 {
     int n = 3; // Size of the triangle pattern
-
+    int num = 1;
     for (int i = 0; i <= n - 1; i++)
     {
         for (int j = i + 1; j >= 1; j--)
         {
-            cout << j << " ";
+            cout << num << " ";
+            num++;
         }
         cout << endl;
     }
--- pattern/reverse_triangle2.cpp
+++ pattern/reverse_triangle2.cpp
@@ -4,12 +4,14 @@ using namespace std;
 int main()
 {
     int n = 3; // Size of the triangle pattern
+    char ch = 'a';
 
     for (int i = 0; i <= n - 1; i++)
     {
         for (int j = i + 1; j >= 1; j--)
         {
-            cout << j << " ";
+            cout << ch << " ";
+            ch++;
         }
         cout << endl;
     }
