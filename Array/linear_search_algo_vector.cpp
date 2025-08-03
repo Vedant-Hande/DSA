fc9055dca30dd877365857fba0931629e2b492cb
vedant hande <vedanthande453@gmail.com>
2025-08-03 11:43:38 +0530

    linear_search_algo_vector

---
+++ Array/linear_search_algo_vector.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int searchNumber(vector<int> &nums, int target)
+{
+    for (int val : nums)
+    {
+        if (val == target)
+        {
+            return val;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int target = 2;
+    vector<int> nums = {
+        4, 1, 2, 1, 2};
+    cout << "target :" << searchNumber(nums, target);
+}
